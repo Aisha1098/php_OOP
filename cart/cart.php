f87cf513cc460eca3084f13be11171c9c54213a8
Aisha1098 <112175138+Aisha1098@users.noreply.github.com>
2023-03-30 10:10:27 +0500

    Add files via upload

---
+++ cart/cart.php
@@ -0,0 +1,31 @@
+<?php
+
+require('product.php');
+
+class Cart{
+    private $products = array();
+
+    public function add(Product $product){
+        $this->products[] = $product;
+    }
+    public function remove(Product $product){
+        foreach($this->products as $key => $p){
+            if($p->getId() === $product->getId()){
+                unset($this->products[$key]);
+                break;
+            }
+        }
+    }
+    public function totalPrice(){
+        $totalprice = 0;
+        if(! empty($this->products)){
+            foreach($this->products as $key => $p){
+                $totalprice += $p->getPrice();
+            }
+        }
+        return $totalprice;
+    }
+    public function clear(){
+        $this->products = [];
+    }
+}
---
+++ cart/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//Suppose you are building a simple e-commerce website that sells products online.
+//You have a Product class that represents a product and you need to create a Cart class that represents a shopping cart.
+// The Cart class should have the following functionalities:
+
+//1. Add a product to the cart.
+//2. Remove a product from the cart.
+//3. Get the total price of all products in the cart.
+// 4. Clear the cart (remove all products).
+
+// Write the Cart class with the above functionalities using PHP object-oriented programming concepts.
+
